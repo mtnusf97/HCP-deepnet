dccf9a1a969360c47d710f1fdee1cd34e3508539
Matin Yousefabadi <mtnusf97@gmail.com>
2021-10-14 09:49:02 +0330

    update config/config_maker.py

--- config/config_maker.py
+++ config/config_maker.py
@@ -64,5 +64,7 @@ for measure in behav_measures:
     base_yaml['dataset']['data_folder_path'] = '/home/yousefabadi/amir/prepared_data'
     base_yaml['dataset']['idx2names_path_train'] = '/home/yousefabadi/amir/data_idx2name_train.pkl'
     base_yaml['dataset']['idx2names_path_test'] = '/home/yousefabadi/amir/data_idx2name_test.pkl'
+    base_yaml['train']['batch_size'] = 40
+    base_yaml['train']['display_iter'] = 1
     with io.open('liangwei_' + measure + '.yaml', 'w', encoding='utf8') as outfile:
         yaml.dump(base_yaml, outfile, default_flow_style=False, allow_unicode=True)
